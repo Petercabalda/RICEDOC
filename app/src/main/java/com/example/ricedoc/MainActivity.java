1e27203ebcf1e53a8e3a51ad0909e374d7fc3579
Peter Alfred D. Cabalda <petercabalda15@gmail.com>
2023-04-04 20:49:29 +0800

    Initial

---
+++ app/src/main/java/com/example/ricedoc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ricedoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, page2.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
