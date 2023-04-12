f696a4947f8284dd17544fb676c307b0cc86c303
Peter Alfred D. Cabalda <petercabalda15@gmail.com>
2023-04-12 11:30:58 +0800

    Initial

--- app/src/main/java/com/example/ricedoc/page2.java
+++ app/src/main/java/com/example/ricedoc/page2.java
@@ -1,14 +1,56 @@
 package com.example.ricedoc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class page2 extends AppCompatActivity {
+
+Button selectBtn,captureBtn;
+Bitmap bitmap;
+ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+
+        selectBtn = findViewById(R.id.selectBtn);
+        captureBtn = findViewById(R.id.captureBtn);
+        imageView = findViewById(R.id.imageView);
+
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(data!=null){
+            Uri uri = data.getData() ;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
