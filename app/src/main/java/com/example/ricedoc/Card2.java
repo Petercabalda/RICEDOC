4948564200a8774a74e041285acaed30990587da
Peter Alfred D. Cabalda <petercabalda15@gmail.com>
2023-05-15 15:11:20 +0800

    May15

---
+++ app/src/main/java/com/example/ricedoc/Card2.java
@@ -0,0 +1,20 @@
+package com.example.ricedoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Card2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_card2);
+    }
+}
--- app/src/main/java/com/example/ricedoc/guideActivity.java
+++ app/src/main/java/com/example/ricedoc/guideActivity.java
@@ -16,7 +16,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class guideActivity extends AppCompatActivity implements View.OnClickListener {
 
-private CardView Card1;
+private CardView Card1, Card2, Card3;
 
     private BottomNavigationView bottomNavigationView;
     @SuppressLint("MissingInflatedId")
@@ -30,6 +30,10 @@ private CardView Card1;
 
         Card1 = (CardView) findViewById(R.id.card1);
         Card1.setOnClickListener((View.OnClickListener)this);
+        Card2 = (CardView) findViewById(R.id.card2);
+        Card2.setOnClickListener((View.OnClickListener)this);
+        Card3 = (CardView) findViewById(R.id.card3);
+        Card3.setOnClickListener((View.OnClickListener)this);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -71,6 +75,15 @@ private CardView Card1;
               i = new Intent(this,Card1.class);
               startActivity(i);
               break;
+          case R.id.card2:
+              i = new Intent(this,Card2.class);
+              startActivity(i);
+              break;
+          case R.id.card3:
+              i = new Intent(this,Card3.class);
+              startActivity(i);
+              break;
       }
+
     }
 }
